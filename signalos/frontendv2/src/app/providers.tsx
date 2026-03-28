4de5d9589223123a646811388401942a6d9e1bc1
Aiden Taylor <aidenjt@uci.edu>
2026-03-28 09:39:14 -0700

    Updating the alert popup to only appear on the calls page.

--- signalos/frontendv2/src/app/providers.tsx
+++ signalos/frontendv2/src/app/providers.tsx
@@ -2,13 +2,7 @@
 
 import type { ReactNode } from "react";
 import { SignalOSProvider } from "../context/SignalOSContext";
-import AlertPopup from "../components/AlertPopup";
 
 export function Providers({ children }: { children: ReactNode }) {
-  return (
-    <SignalOSProvider>
-      {children}
-      <AlertPopup />
-    </SignalOSProvider>
-  );
+  return <SignalOSProvider>{children}</SignalOSProvider>;
 }
